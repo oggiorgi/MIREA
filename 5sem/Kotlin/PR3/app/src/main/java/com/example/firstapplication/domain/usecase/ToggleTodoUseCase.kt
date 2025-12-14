3daf3e2b32dee2480b2d1d778a15943fa409245d
oggiorgi <ranertop@gmail.com>
2025-12-14 21:28:29 +0300

    чистка

--- 5sem/Kotlin/PR3/app/src/main/java/com/example/firstapplication/domain/usecase/ToggleTodoUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.firstapplication.domain.usecase
-
-import com.example.firstapplication.domain.repository.TodoRepository
-
-class ToggleTodoUseCase(private val repository: TodoRepository) {
-    suspend operator fun invoke(id: Int) = repository.toggleTodo(id)
-}
-
